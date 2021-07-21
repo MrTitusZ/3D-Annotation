e01700c2279b0d550a47175068ab4325cc0b6d94
MrTitusZ <novak.csaba.titusz@gmail.com>
2021-07-21 09:56:39 +0200

    Quick Fixes

--- 3D Annotation/Assets/Scripts/BoundingBox.cs
+++ 3D Annotation/Assets/Scripts/BoundingBox.cs
@@ -19,6 +19,32 @@ public class BoundingBox : MonoBehaviour
         GUI.Box(GUI2dRectWithObject(gameObject), "");
     }
 
+    public static Rect GUI3dRectWithObject(GameObject go)
+    {
+
+        Vector3 cen = go.GetComponent<Renderer>().bounds.center;
+        Vector3 ext = go.GetComponent<Renderer>().bounds.extents;
+        Vector2[] extentPoints = new Vector2[8]
+        {
+            WorldToGUIPoint(new Vector3(cen.x-ext.x, cen.y-ext.y, cen.z-ext.z)),
+            WorldToGUIPoint(new Vector3(cen.x+ext.x, cen.y-ext.y, cen.z-ext.z)),
+            WorldToGUIPoint(new Vector3(cen.x-ext.x, cen.y-ext.y, cen.z+ext.z)),
+            WorldToGUIPoint(new Vector3(cen.x+ext.x, cen.y-ext.y, cen.z+ext.z)),
+            WorldToGUIPoint(new Vector3(cen.x-ext.x, cen.y+ext.y, cen.z-ext.z)),
+            WorldToGUIPoint(new Vector3(cen.x+ext.x, cen.y+ext.y, cen.z-ext.z)),
+            WorldToGUIPoint(new Vector3(cen.x-ext.x, cen.y+ext.y, cen.z+ext.z)),
+            WorldToGUIPoint(new Vector3(cen.x+ext.x, cen.y+ext.y, cen.z+ext.z))
+        };
+        Vector2 min = extentPoints[0];
+        Vector2 max = extentPoints[0];
+        foreach (Vector2 v in extentPoints)
+        {
+            min = Vector2.Min(min, v);
+            max = Vector2.Max(max, v);
+        }
+        return new Rect(min.x, min.y, max.x - min.x, max.y - min.y);
+    }
+
     public static Rect GUI2dRectWithObject(GameObject go)
     {
         Vector3[] vertices = go.GetComponent<MeshFilter>().mesh.vertices;
