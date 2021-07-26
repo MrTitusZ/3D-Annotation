917494224f8e06dad5d69acce72bcc1c6981d115
MrTitusZ <novak.csaba.titusz@gmail.com>
2021-07-26 13:07:02 +0200

    Added Snapshot

--- 3D Annotation/Assets/Scripts/RotateObject.cs
+++ 3D Annotation/Assets/Scripts/RotateObject.cs
@@ -6,17 +6,11 @@ public class RotateObject : MonoBehaviour
 {
     [SerializeField] float rotationDegree = 15f;
 
-    void Start()
-    {
-
-    }
-
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.Space))
         {
             RotateGameObject();
-            Debug.Log("Space pressed!");
         }
     }
 
--- 3D Annotation/Assets/Scripts/TakeSnapshot.cs
+++ 3D Annotation/Assets/Scripts/TakeSnapshot.cs
@@ -4,5 +4,37 @@ using UnityEngine;
 
 public class TakeSnapshot : MonoBehaviour
 {
+    [SerializeField] public int resWidth = 1920;
+    [SerializeField] public int resHeight = 1080;
 
+    Camera snapshotCamera;
+    int snapshotIndex = 0;
+
+    void Start()
+    {
+        snapshotCamera = gameObject.GetComponent<Camera>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("k"))
+        {
+            CaptureSnapshot();
+        }
+    }
+
+    private void CaptureSnapshot()
+    {
+        RenderTexture rt = new RenderTexture(resWidth, resHeight, 24);
+        snapshotCamera.targetTexture = rt;
+        Texture2D screenShot = new Texture2D(resWidth, resHeight, TextureFormat.RGB24, false);
+        snapshotCamera.Render();
+        RenderTexture.active = rt;
+        screenShot.ReadPixels(new Rect(0, 0, resWidth, resHeight), 0, 0);
+        snapshotCamera.targetTexture = null;
+        byte[] bytes = screenShot.EncodeToPNG();
+        string filename = "C:/Users/humanscanner/Desktop/Pictures/" + snapshotIndex + ".png";
+        System.IO.File.WriteAllBytes(filename, bytes);
+        snapshotIndex++;
+    }
 }
